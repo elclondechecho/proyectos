ac9f54335305df6e6c4f75971b5b2a0547529d02
elclondechecho <sergiofabianev@gmail.com>
2024-09-10 19:13:49 -0500

    Update estudiante.py

--- estudiante.py
+++ estudiante.py
@@ -1,2 +1,21 @@
-print("hola mundo")
-print("leroy")
+class Estudiante:
+    def __init__(self, nombre="", edad=0, semestre=""):
+        self.nombre = nombre
+        self.edad = edad
+        self.semestre = semestre
+
+    def ingresarDatos(self):
+        self.nombre = input("ingrese el nombre del estudiante: ")
+        self.edad = int(input("ingrese la edad del estudiante: "))
+        self.semestre = input("ingrese el semestre del estudiante: ")
+
+    def imprimirDatos(self):
+        print(f"nombre: {self.nombre}")
+        print(f"edad: {self.edad}")
+        print(f"semestre: {self.semestre}")
+
+    def matricular(self):
+        print(f"{self.nombre} ha sido matriculado.")
+
+    def pagarPension(self):
+        print(f"{self.nombre} ha pagado la pensión.")
