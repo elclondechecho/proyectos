6d8001bc15207f1d998795717950962218e5d3e0
elclondechecho <sergiofabianev@gmail.com>
2024-09-10 20:26:12 -0500

    Create ejecucion.py

---
+++ ejecucion.py
@@ -0,0 +1,20 @@
+# ejecucion.py
+
+from estudiante import Estudiante
+
+def main():
+    estudiante = Estudiante(nombre="sergio", edad=18, semestre="primer semestre")
+    
+    print("Datos iniciales del estudiante:")
+    estudiante.imprimirDatos()
+    
+    estudiante.ingresarDatos()
+    
+    print("Datos después de ingresar nuevos datos:")
+    estudiante.imprimirDatos()
+    
+    estudiante.matricular()
+    estudiante.pagarPension()
+
+if __name__ == "__main__":
+    main()
