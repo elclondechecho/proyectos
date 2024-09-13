048bd92dbab1e75965be80f5eed41fe63a7d0cd8
elclondechecho <sergiofabianev@gmail.com>
2024-09-13 00:32:29 -0500

    Update ejecucion2.py

--- ejecucion2.py
+++ ejecucion2.py
@@ -1,19 +1,24 @@
 from estudiante import Estudiante
 
 def main():
-
     estudiantes = [
-        Estudiante("patrick", 19, "primer semestre"),
+        Estudiante("Patrick", 19, "primer semestre"),
         Estudiante("Luis", 23, "segundo semestre"),
         Estudiante("Maria", 27, "quinto semestre")
     ]
-
 
     for estudiante in estudiantes:
         estudiante.imprimirDatos()
         estudiante.matricular()
         estudiante.pagarPension()
         print()
+
+
+    print(Estudiante.esMayorDeEdad(16)) 
+    print(Estudiante.saludo())
+
+
+    print(Estudiante.cantidadEstudiantes())
 
 if __name__ == "__main__":
     main()
